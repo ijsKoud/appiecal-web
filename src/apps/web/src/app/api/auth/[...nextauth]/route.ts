f34a831933a6552036c2ffa4385958a8b9dfb5fc
Daan <daan@klrnbk.nl>
2026-04-29 00:37:00 +0200

    fix: missing scope to add scope to claims

--- src/apps/web/src/app/api/auth/[...nextauth]/route.ts
+++ src/apps/web/src/app/api/auth/[...nextauth]/route.ts
@@ -3,6 +3,21 @@ import AuthentikProvider from "next-auth/providers/authentik";
 
 const handler = NextAuth({
 	secret: process.env.NEXTAUTH_SECRET,
+	callbacks: {
+		async jwt({ token, account }) {
+			if (account) {
+				token.idToken = account.id_token;
+				token.accessToken = account.access_token;
+			}
+
+			return token;
+		},
+
+		async session({ session, token }) {
+			session.idToken = token.idToken as string;
+			return session;
+		}
+	},
 	providers: [
 		AuthentikProvider({
 			clientId: process.env.AUTHENTIK_CLIENT_ID!,
@@ -10,7 +25,7 @@ const handler = NextAuth({
 			issuer: process.env.AUTHENTIK_ISSUER!,
 			authorization: {
 				params: {
-					scope: "openid profile email https://klrnbk.nl/projects/appiecal:use"
+					scope: "openid scope profile email https://klrnbk.nl/projects/appiecal:use"
 				}
 			}
 		})
