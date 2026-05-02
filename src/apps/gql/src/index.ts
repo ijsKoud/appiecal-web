1915ad4ed103e98425c20ebbf9509fa8d1d4f073
Daan Klarenbeek <daan@klrnbk.nl>
2026-05-02 12:41:54 +0200

    feat(Gql): add calendar module (#20)
    * feat(Gql): add calendar API client
    
    * feat: add gql types for calendar module
    
    * feat: add module and provider for calendar
    
    * feat: add query and mutation resolvers

--- src/apps/gql/src/index.ts
+++ src/apps/gql/src/index.ts
@@ -15,6 +15,7 @@ import {
 	IdpApiFetchClientOptionsProvider
 } from "#providers/api-fetch-client-options/provider.js";
 import { idpModule } from "#modules/idp/module.js";
+import { calendarModule } from "#modules/calendar/module.js";
 
 interface Context {
 	token?: String;
@@ -28,7 +29,7 @@ const application = createApplication({
 		CalendarApiFetchClientOptionsProvider,
 		IdpApiFetchClientOptionsProvider
 	],
-	modules: [baseModule, syncModule, idpModule]
+	modules: [baseModule, syncModule, idpModule, calendarModule]
 });
 
 // Workaround until official Apollo 4 support: https://github.com/graphql-hive/graphql-modules/issues/2270#issuecomment-1365000062
