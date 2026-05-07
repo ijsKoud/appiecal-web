f642e178dca0558f47f70af896af16006fe9bb6f
Daan Klarenbeek <daan@klrnbk.nl>
2026-05-07 10:42:08 +0200

    feat: add sync status to landing page (#27)

---
+++ src/apps/web/src/components/status.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import { FC } from "react";
+import { useIsAllowedToSync } from "./navigation/action-header/hooks/is-allowed-to-sync-hook";
+
+export const Status: FC = () => {
+	const isAllowedToSync = useIsAllowedToSync();
+
+	return (
+		<div>
+			<h2 className="text-xl font-bold">Status</h2>
+			<p>{isAllowedToSync ? "@AH Account and calendar linked" : "Missing @AH account and calendar link"}</p>
+		</div>
+	);
+};
