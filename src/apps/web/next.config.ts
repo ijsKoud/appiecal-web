026aa0078dbfa6a1511f81b65ae8ccb1eb046d53
Daan Klarenbeek <daan@klrnbk.nl>
2026-05-08 11:00:27 +0200

    fix(Web): next builds did not finish due to root issues (#34)
    * fix: revert pnpm to v10
    
    * fix(Web): next builds did not finish due to root issues

--- src/apps/web/next.config.ts
+++ src/apps/web/next.config.ts
@@ -1,9 +1,13 @@
 import type { NextConfig } from "next";
+import { resolve } from "path";
+
+const repoRoot = process.env.NX_WORKSPACE_ROOT ? process.env.NX_WORKSPACE_ROOT : resolve(__dirname, "../../");
 
 const nextConfig: NextConfig = {
 	output: "standalone",
 	reactStrictMode: true,
-	distDir: "dist"
+	distDir: "dist",
+	outputFileTracingRoot: repoRoot
 };
 
 export default nextConfig;
